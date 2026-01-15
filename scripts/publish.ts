bc68abe13c792be693a7dac570e572b25763bea2
hboot <bobolity@163.com>
2026-01-15 14:34:34 +0800

    chore(publish): 启用 npm provenance 证明功能
    在 scripts/publish.ts 中为 pnpm publish 命令添加
    --provenance 参数，以启用包来源证明功能

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,7 +3,7 @@ import pkg from "../package.json" with { type: "json" };
 
 const { version } = pkg;
 
-let command = "pnpm publish --access public --no-git-checks";
+let command = "pnpm publish --provenance --access public --no-git-checks";
 
 if (version.includes("beta")) command += " --tag beta";
 
